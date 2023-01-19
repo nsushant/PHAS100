739cbebda0ec25322fc5cd6d3c58b4ded1938faf
sushanta nigudkar <nsushant683@gmail.com>
2023-01-19 10:14:28 +0000

    Initial Commit

---
+++ phas100/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  phas100
+//
+//  Created by Sushant Nigudkar on 19/01/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
