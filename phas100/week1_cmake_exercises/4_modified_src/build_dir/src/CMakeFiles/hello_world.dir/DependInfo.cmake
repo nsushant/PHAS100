9550c0008f6b2addada7e5b130c3c9829e2805ce
sushanta nigudkar <nsushant683@gmail.com>
2023-01-19 13:59:26 +0000

    c-make exercise 6

---
+++ phas100/week1_cmake_exercises/4_modified_src/build_dir/src/CMakeFiles/hello_world.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/sushantnigudkar/desktop/phas100/phas100/week1_cmake_exercises/4_modified_src/src/hello.cpp" "src/CMakeFiles/hello_world.dir/hello.cpp.o" "gcc" "src/CMakeFiles/hello_world.dir/hello.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
